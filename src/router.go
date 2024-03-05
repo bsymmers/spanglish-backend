44c28e743e2107877383f38206a87586c2fc8cc6
Brandon Symmers <brandon.symmers@gmail.com>
2024-03-04 23:58:11 -0800

    Revert "added deepl api integration"
    This reverts commit 8f0e01a683c2c227b747608bb309b8a5547296e7.

--- src/router.go
+++ src/router.go
@@ -11,12 +11,6 @@ type sourceText struct {
 	Source      string `form:"Source" binding:"required"`
 	Target      string `form:"Target" binding:"required"`
 	PostContent string `form:"postContent" binding:"required"`
-	Deepl       bool   `form:"Deepl" binding:"required"`
-}
-
-type ResponseObj struct {
-	CognateResponse string
-	DeeplResponse   string
 }
 
 func routing() {
@@ -25,7 +19,6 @@ func routing() {
 	// router.Use(ResponseLogger())
 	router.Use(cors.Default())
 	router.POST("/sourceText", postsourceText)
-	// router.POST("/deepl", deeplHandler)
 
 	router.Run("localhost:8080")
 }
@@ -40,26 +33,6 @@ func postsourceText(c *gin.Context) {
 
 	// DO something with request body thats now store in newSourceText
 	response := handleTranslation(newSourceText)
-
-	var responseObj ResponseObj
-	var respCode int
-	responseObj.CognateResponse = response.retStr
-
-	if (newSourceText.Deepl) && (response.respCode == 200) {
-		deeplResponse := deeplHandler(newSourceText)
-		responseObj.DeeplResponse = deeplResponse.retStr
-
-		if deeplResponse.respCode == 400 {
-			respCode = 400
-		} else {
-			respCode = 200
-		}
-
-	} else {
-		responseObj.DeeplResponse = ""
-		respCode = 200
-	}
-
-	c.JSON(respCode, responseObj)
+	c.IndentedJSON(response.respCode, response.retStr)
 
 }
